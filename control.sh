be6bfe6ac1985c7a341bbf5313f3059a380ea6bc
Vishnu Bhagirathan <vishnub341@gmail.com>
2026-08-17 17:29:53 +0530

    Fix device identifier for tracxn-lp-599 to tracxn-lp-811

--- control.sh
+++ control.sh
@@ -135,7 +135,7 @@ case "$USER_LOWER" in
         IDENTITY="Device-4230"
         [ -z "$TUNNEL_TOKEN" ] && TUNNEL_TOKEN="$TUNNEL_TOKEN_4230"
         ;;
-    "tracxn-lp-599")
+    "tracxn-lp-811")
         IDENTITY="Device-599"
         [ -z "$TUNNEL_TOKEN" ] && TUNNEL_TOKEN="$TUNNEL_TOKEN_599"
         ;;
